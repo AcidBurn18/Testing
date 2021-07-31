951050a281a4f885c3244fa893000acd0b1b580a
Ansh <iansh2118@gmail.com>
2021-07-31 23:29:41 +0530

    Update streamlitcheck.py

--- streamlitcheck.py
+++ streamlitcheck.py
@@ -1,14 +1,14 @@
 import streamlit as st
+st.write("Point Of View")
 def main():
-	st.title("Awesome Streamlit for ML")
-	st.subheader("How to run streamlit from colab")
-	menu = ["Home","About"]
-	choice = st.sidebar.selectbox('Menu',menu)
-	if choice == 'Home':
-		st.subheader("Streamlit From Colab")	
-if __name__ == '__main__':
+  form=st.form(key='my_from')
+  username=st.text_input(label='Username')
+  submit=form.form_submit('Go')
+  
+  if (submit):
+    f=pd.DataFrame(username,index=[0])
+    return username
+  else:
+    st.write("Error")
+if __name__=='__main__':
 	main()
-
-
-
- 
