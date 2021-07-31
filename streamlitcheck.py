89b423aab09271e7bbab45559d83b48d09d08625
Ansh <iansh2118@gmail.com>
2021-07-31 23:51:37 +0530

    Update streamlitcheck.py

--- streamlitcheck.py
+++ streamlitcheck.py
@@ -3,7 +3,7 @@ st.write("Point Of View")
 def main():
   form=st.form(key='my_form')
   username=st.text_input(label='Username')
-  submit=form.form_submit('Go')
+  submit=form.form_submit_button(label='Go')
   
   if (submit):
     f=pd.DataFrame(username,index=[0])
